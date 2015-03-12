b300f09af553f2a221a80a633e4443ee26d3988c
Faiz Shukri <faizshukri90@gmail.com>
2015-03-12 20:40:07 +0000

    Prepare container for section and update the menu highlight
    Signed-off-by: Faiz Shukri <faizshukri90@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
-jQuery(document).ready(function($){
-    
-});
+(function(){
+    jQuery(document).ready(function($){
+        $('#main-navbar').find('li').on('click', function(e){
+
+            // Update main menu
+            $('#main-navbar').find('li').removeClass('active');
+            $(this).addClass('active');
+
+            // Open and close related section
+            $('section.active').hide().removeClass('active');
+            $($(this).data('target')).show().addClass('active');
+            
+        });
+    });
+})();
