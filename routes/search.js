4da6c9437e6097ef4427373ad90f937d003750f8
Faiz Shukri <faizshukri90@gmail.com>
2015-03-26 22:18:21 +0000

    Refactor codes

--- routes/search.js
+++ routes/search.js
@@ -1,46 +1,19 @@
 var express = require('express');
 var router = express.Router();
-
-var Twitter = require('twit');
-var client = new Twitter({
-    consumer_key        : config.app.twitter.consumer_key,
-    consumer_secret     : config.app.twitter.consumer_secret,
-    access_token    : config.app.twitter.access_token,
-    access_token_secret : config.app.twitter.access_token_secret
-});
-
-// console.log(config.app.twitter.consumer_key.toString());
-// console.log(config.app.twitter.consumer_key, config.app.twitter.consumer_secret, config.app.twitter.access_token, config.app.twitter.access_token_secret);
-
+var twitter = require('../lib/twitter');
+var search = require('../lib/search');
 
 /* GET tweet page. */
 router.get('/places', function(req, res, next) {
     var location = req.query.location;
     if(location){
-        client.get('geo/search', { query: location, max_results: 5 }, function(error, places){
+        twitter.get('geo/search', { query: location, max_results: 5 }, function(error, places){
           if(error) throw error;
-          places = pluckselect2( places.result.places, ['id', 'full_name'] );
+          places = search.pluckselect2( places.result.places, ['id', 'full_name'] );
           res.send(places);
         });
     }
   
 });
-
-function search_name(name){
-
-}
-
-function search_coordinates(latitude, longitude){
-
-}
-
-function pluckselect2(obj, key) {
-    return obj.map(function(elem){
-        return {
-            id   : elem[key[0]],
-            text : elem[key[1]]
-        };
-    });
-};
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../lib/db');
+var user = require('../lib/user');
 
 /* GET user page. */
 router.get('/', function(req, res, next) {
   res.render('users/index', { path: 'user'});
 });
+
+router.get('/interesting-venues', function(req, res, next) {
+  res.render('users/interesting_venues', { path: 'user'});
+});
+
+router.get('/venue-visitors', function(req, res, next) {
+  var params = req.query;
+  user.getVenueVisitors(params.location, params.days_limit, function(data){
+    // res.send(data);
+    res.render('users/venue_visitors', { path: 'user'});
+  });
+});
+
+
 
 module.exports = router;
