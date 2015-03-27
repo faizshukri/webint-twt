2c7f66d2e21199afaa2f3f142127040ac209508e
Faiz Shukri <faizshukri90@gmail.com>
2015-03-27 15:18:31 +0000

    Add user profile page
    Signed-off-by: Faiz Shukri <faizshukri90@gmail.com>

--- routes/user.js
+++ routes/user.js
@@ -9,7 +9,24 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/profile/:username', function(req, res, next) {
-  res.render('users/profile', { path: 'user'});
+
+  var username = req.params.username;
+
+  user.getUserTweets(username, function(data){
+
+    // If the user has tweet, grab the user object from the first tweet
+    if(data.statuses.length > 0){
+        var author = data.statuses[0].user;
+        res.render('users/profile', { path: 'user', author: author, tweets: data.statuses });
+    // If the user does not have any tweet, search from twitter again
+    } else {
+        user.getUser(username, function(data){
+          var author = data;
+          res.render('users/profile', { path: 'user', author: author, tweets: [] });
+        });
+    }
+  });
+  
 });
 
 router.get('/interesting-venues', function(req, res, next) {
