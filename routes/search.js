6c155afe4041dac2dfe4379eed07028ddabd89c8
Faiz Shukri <faizshukri90@gmail.com>
2015-04-02 16:05:29 +0100

    Move pluckselect2 function to utils file
    Signed-off-by: Faiz Shukri <faizshukri90@gmail.com>

--- routes/search.js
+++ routes/search.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var twitter = require('../lib/twitter');
-var search = require('../lib/search');
+var utils = require('../lib/utils');
 
 /* GET tweet page. */
 router.get('/places', function(req, res, next) {
@@ -9,7 +9,7 @@ router.get('/places', function(req, res, next) {
     if(location){
         twitter.get('geo/search', { query: location, max_results: 10 }, function(error, places){
           if(error) throw error;
-          places = search.pluckselect2( places.result.places, ['id', 'full_name'] );
+          places = utils.pluckselect2( places.result.places, ['id', 'full_name'] );
           res.send(places);
         });
     }
