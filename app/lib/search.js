3d92bcb8f9d2cc77beeb025628d192fa77883973
Faiz Shukri <faizshukri90@gmail.com>
2015-04-02 17:47:13 +0100

    Refactor search
    Signed-off-by: Faiz Shukri <faizshukri90@gmail.com>

--- app/lib/search.js
+++ app/lib/search.js
@@ -1,11 +1,12 @@
+var twitter = require('../lib/twitter');
+
 var search = {};
 
-search.search_name = function(name){
-
-}
-
-search.search_coordinates = function(latitude, longitude){
-
+search.getPlaces = function(location, count, callback){
+    twitter.get('geo/search', { query: location, max_results: 10 }, function(err, data, response){
+      if(err) throw err;
+      callback(data);
+    });
 }
 
 module.exports = search;
--- app/routes/search.js
+++ app/routes/search.js
@@ -1,19 +1,18 @@
-var express = require('express');
-var router = express.Router();
-var twitter = require('../lib/twitter');
-var utils = require('../lib/utils');
+var express = require('express'),
+    router  = express.Router();
+
+var utils  = require('../lib/utils'),
+    search = require('../lib/search');
 
 /* GET tweet page. */
 router.get('/places', function(req, res, next) {
     var location = req.query.location;
     if(location){
-        twitter.get('geo/search', { query: location, max_results: 10 }, function(error, places){
-          if(error) throw error;
-          places = utils.pluckselect2( places.result.places, ['id', 'full_name'] );
-          res.send(places);
+        search.getPlaces(location, 10, function(places){
+            places = utils.pluckselect2( places.result.places, ['id', 'full_name'] );
+            res.send(places);
         });
     }
-  
 });
 
 module.exports = router;
