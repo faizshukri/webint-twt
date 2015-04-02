c24d10217c69ee5f72aae7e64dc6697b183c781b
QAini <chocetti@gmail.com>
2015-04-02 16:52:50 +0100

    Initial user interface for tweet/index and initial stages of lib/tweet.js and routes/tweet.js

---
+++ app/lib/tweet.js
@@ -0,0 +1,11 @@
+var twitter = require('./twitter');
+
+var tweet = {};
+
+tweet.searchKeywordTweets = function(callback){
+    twitter.get('search/tweets', {q: 'asdfghjkl'}, function(err, data, response){
+        callback(data);
+    });
+}
+
+module.exports = tweet;
--- routes/tweet.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET tweet page. */
-router.get('/', function(req, res, next) {
-  res.render('tweets/index', { path: 'tweet'});
-});
-
-module.exports = router;
