4dcce3aba4b8f75a6e9e9e4dca0a36879817d80b
Faiz Shukri <faizshukri90@gmail.com>
2015-04-13 19:57:02 +0100

    Add twitter_id to user
    Signed-off-by: Faiz Shukri <faizshukri90@gmail.com>

--- migrations/20150412160724-add-users.js
+++ migrations/20150412160724-add-users.js
@@ -4,6 +4,7 @@ var type = dbm.dataType;
 exports.up = function(db, callback) {
   db.createTable('users', {
     id: { type: 'int', primaryKey: true, autoIncrement: true },
+    twitter_id: 'string',
     name: 'string',
     description: 'string',
     photo: 'string',
