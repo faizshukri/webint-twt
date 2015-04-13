b64e4f3f61df8df35b06862045ff04532d7a0b20
Faiz Shukri <faizshukri90@gmail.com>
2015-04-13 01:35:36 +0100

    Fix socket using coordinate
    Signed-off-by: Faiz Shukri <faizshukri90@gmail.com>

--- app/lib/socketio.js
+++ app/lib/socketio.js
@@ -6,35 +6,43 @@ var socketio = {};
 
 socketio.connection = function(socket){
 
-    socket.on('start_stream', function(location_id){
+    socket.on('start_stream', function(options){
 
-        twitter.get('geo/id/:id', { id: location_id }, function(err, data, response){
+        if(options.location){
+            twitter.get('geo/id/:id', { id: options.location }, function(err, data, response){
+                var x = [], y = [];
+                data.bounding_box.coordinates[0].forEach(function(location){
+                    x.push(location[0]),
+                    y.push(location[1]);
+                });
 
-            var x = [], y = [];
+                x = utils.removeDuplicateValuesInArray(x);
+                y = utils.removeDuplicateValuesInArray(y);
 
-            data.bounding_box.coordinates[0].forEach(function(location){
-                x.push(location[0]),
-                y.push(location[1]);
+                emitPlaces(socket, [ x[0], y[0], x[1], y[1] ]);
             });
-
-            x = utils.removeDuplicateValuesInArray(x);
-            y = utils.removeDuplicateValuesInArray(y);
-
-            var stream = twitter.stream('statuses/filter', { locations: [ x[0], y[0], x[1], y[1] ] });
-
-            var user_ids = [];
-
-            stream.on('tweet', function(tweet){
-
-                // Prevent sending same user
-                if( user_ids.indexOf(tweet.user.id) > -1 ) return;
-                user_ids.push(tweet.user.id);
-
-                socket.emit('tweet', tweet);
-            });
-        });
+        } else if(options.coordinates) {
+            options.coordinates = options.coordinates.split(',');
+            emitPlaces(socket, [parseFloat(options.coordinates[1]) - 0.1, parseFloat(options.coordinates[0]) - 0.1, parseFloat(options.coordinates[1]) + 0.1, parseFloat(options.coordinates[0]) + 0.1]);
+        }
     });
 
+}
+
+function emitPlaces(socket, coordinates){
+
+    var stream = twitter.stream('statuses/filter', { locations: coordinates });
+
+    var user_ids = [];
+
+    stream.on('tweet', function(tweet){
+
+        // Prevent sending same user
+        if( user_ids.indexOf(tweet.user.id) > -1 ) return;
+        user_ids.push(tweet.user.id);
+
+        socket.emit('tweet', tweet);
+    });
 }
 
 module.exports = socketio;
